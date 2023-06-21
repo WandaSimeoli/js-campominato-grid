2a1ecd1e88c0f3073106af8366a9ad9c85400844
WandaSimeoli <wandasimeoli@gmail.com>
2023-06-21 18:26:48 +0200

    Stampa dei numeri con ciclo for

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 // Creare un bottone per generare una griglia quadrata;
-// Creare una griglia quadrata con 10 caselle numerate per 10 righe ( tot 100);
+// Creare una griglia quadrata con 10 caselle numerate per 10 righe (tot 100);
 // Al click dell'utente sulla cella, la cella cliccata si colora di azzurro;
 // Al click dell'utente stampare un messaggio in console con il numero della cella cliccata;
 
 // Creare la griglia numerata 
 
+for ( let i = 1; i <= 100; i++ ) {
+
+    const containerGrid = document.querySelector('.container-grid');
+const grid = document.createElement ('div');
+containerGrid.append(grid);
+grid.innerHTML = (i);
+
+}
+
+
+
+    
