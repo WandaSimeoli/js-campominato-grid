b05437955b5f3e3897d562f31e03b419e554d474
WandaSimeoli <wandasimeoli@gmail.com>
2023-06-21 17:46:49 +0200

    Inizializzazione progetto

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Creare un bottone per generare una griglia quadrata;
+// Creare una griglia quadrata con 10 caselle numerate per 10 righe ( tot 100);
+// Al click dell'utente sulla cella, la cella cliccata si colora di azzurro;
+// Al click dell'utente stampare un messaggio in console con il numero della cella cliccata;
+
+// Creare la griglia numerata 
+
